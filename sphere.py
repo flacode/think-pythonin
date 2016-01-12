2659215f7f312ddc9a12db137ac232adfc96d1af
flacode <flavianshemerirwe@gmail.com>
2016-01-12 10:48:14 +0000

    chapter 2

---
+++ sphere.py
@@ -0,0 +1,7 @@
+#method to find the volume of the sphere given the radius
+from math import pi#using pi from the math module
+def volume(radius):
+    return (4.0/3)*pi*radius**3
+
+
+print volume(5)
